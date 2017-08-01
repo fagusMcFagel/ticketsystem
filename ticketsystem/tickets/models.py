6b21294cc0ddb00109e8d137226cb66946aa935e
forner <falk.forner@googlemail.com>
2017-08-01 16:25:28 +0200

    Added Task regarding creation of validators for model fields

--- ticketsystem/tickets/models.py
+++ ticketsystem/tickets/models.py
@@ -1,7 +1,12 @@
 from django.db import models
 
 # Create your models here.
-class Ticket(models.Model):    
+class Ticket(models.Model):   
+    
+    #
+    # TODO: CREATE VALIDATORS FOR CRITICAL FIELDS
+    # 
+    
     #basic ticket data
     sector = models.CharField(max_length=30)
     category = models.CharField(max_length=30)
@@ -17,6 +22,6 @@ class Ticket(models.Model):
     solution = models.CharField(max_length=400)
     keywords = models.CharField(max_length=100)
     
-    # FIXME: create separate class Person and change to foreign key?
+    # TODO: create separate class Person and change to foreign key?
     responsible_person = models.CharField(max_length=40)
     creator = models.CharField(max_length=40)
